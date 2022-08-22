6ce67c830559d8fc01b6c76a22d73733e49194a9
VIEIGHT <118950924+VIEIGHT@users.noreply.github.com>
2022-08-22 09:03:56 +0000

    fixes and improvements

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div id="app">
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'app'
+
+}
+</script>
+<style>
+#app{
+  width: 100%; height: max-content;
+  position: absolute;background: linear-gradient(135deg,#29323C,#2B5876,#4E4376);background: -webkit-linear-gradient(135deg,#29323C,#2B5876,#4E4376);background: -o-linear-gradient(135deg,#29323C,#2B5876,#4E4376);background: -moz-linear-gradient(135deg,#29323C,#2B5876,#4E4376);background: -ms-linear-gradient(135deg,#29323C,#2B5876,#4E4376);
+}
+
+</style>
