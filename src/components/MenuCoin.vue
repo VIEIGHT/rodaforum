84f82b76bc2a330f4541c8265e88f026c264c78d
VIEIGHT <118950924+VIEIGHT@users.noreply.github.com>
2022-08-28 10:36:33 +0000

    Remove deprecated code.

--- src/components/MenuCoin.vue
+++ src/components/MenuCoin.vue
@@ -9,4 +9,5 @@
     <ul v-for=" (coin) in $store.state.data" :key="coin">
       <router-link :to="`/coin/${coin.id}`" style="display:contents">
     <li class="panel-block" >
-      <span style="font-size: 20px;width: 191px;"> <img :src="`https://cdn.arzdigital.com/uploads/assets/coins/icons/32x32/${coin.id}.png`" width="30" height="30"> {{coin.name}} 
+      <span style="font-size: 20px;width: 191px;"> <img :src="`https://cdn.arzdigital.com/uploads/assets/coins/icons/32x32/${coin.id}.png`" width="30" height="30"> {{coin.name}} </span>
+      <p style="font-size: 20px;">{{coi
