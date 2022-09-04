c2d8574b3673f9f49bb072fea6eef9595651ec48
VIEIGHT <118950924+VIEIGHT@users.noreply.github.com>
2022-09-04 17:09:47 +0000

    Updated Amharic translation.

--- src/components/MenuCoin.vue
+++ src/components/MenuCoin.vue
@@ -12,4 +12,4 @@
       <span style="font-size: 20px;width: 191px;"> <img :src="`https://cdn.arzdigital.com/uploads/assets/coins/icons/32x32/${coin.id}.png`" width="30" height="30"> {{coin.name}} </span>
       <p style="font-size: 20px;">{{coin.priceUsd.slice(0,7)}} $</p>
       <svg v-if="coin.changePercent24Hr <'0'" width="30px" height="30px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title>ionicons-v5-c</title><polyline points="352 368 464 368 464 256" style="fill:none;stroke:red;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/><path d="M48,144,169.37,265.37a32,32,0,0,0,45.26,0l50.74-50.74a32,32,0,0,1,45.26,0L448,352" style="fill:none;stroke:red;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>
-      <svg v-if="coin.changePercent24Hr >'0'" width="30px" height="30px" viewBox="0 0 512 512" xmlns="ht
+      <svg v-if="coin.changePercent24Hr >'0'" width="30px" height="30px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title>ionicons-v5-c</ti
