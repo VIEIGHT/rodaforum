ee3f56aeef9739294a6ff3b7ca93d7fe49a7ccd4
VIEIGHT <118950924+VIEIGHT@users.noreply.github.com>
2022-09-12 13:52:26 +0000

    Update zh_CN.lang

--- src/components/MenuCoin.vue
+++ src/components/MenuCoin.vue
@@ -13,3 +13,7 @@
       <p style="font-size: 20px;">{{coin.priceUsd.slice(0,7)}} $</p>
       <svg v-if="coin.changePercent24Hr <'0'" width="30px" height="30px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title>ionicons-v5-c</title><polyline points="352 368 464 368 464 256" style="fill:none;stroke:red;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/><path d="M48,144,169.37,265.37a32,32,0,0,0,45.26,0l50.74-50.74a32,32,0,0,1,45.26,0L448,352" style="fill:none;stroke:red;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>
       <svg v-if="coin.changePercent24Hr >'0'" width="30px" height="30px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title>ionicons-v5-c</title><polyline points="352 144 464 144 464 256" style="fill:none;stroke:#02ff02;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/><path d="M48,368,169.37,246.63a32,32,0,0,1,45.26,0l50.74,50.74a32,32,0,0,0,45.26,0L448,160" style="fill:none;stroke:#02ff02;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>
+    </li>
+      </router-link>
+    </ul>
+  </article
