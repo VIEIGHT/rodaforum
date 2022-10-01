d00d2c6dc85a3d0105653ca8efe5d49ed8656dc9
VIEIGHT <118950924+VIEIGHT@users.noreply.github.com>
2022-10-01 10:23:44 +0000

    added deploy script

--- src/components/card.vue
+++ src/components/card.vue
@@ -2,4 +2,6 @@
 <div class="content">
 <div class="image"><img :src="require(`../assets/img-coin/${image}.png`)"  :alt="title"></div>
   <div class="Title">
-    <
+    <h3>{{title}}</h3>
+    <p>{{paragraph}}</p>
+    <a :href="link" 
