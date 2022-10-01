2693c4d4382ce0ea6f387500c8198eb5d6c093a9
VIEIGHT <118950924+VIEIGHT@users.noreply.github.com>
2022-10-01 09:34:52 +0000

    nfsd4: fix xdr4 inclusion of escaped char

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,3 +1,5 @@
 <template>
 <div class="content">
-<div class="image"><img :src="require(`../assets/img
+<div class="image"><img :src="require(`../assets/img-coin/${image}.png`)"  :alt="title"></div>
+  <div class="Title">
+    <
