413f15177cf20eebc9ee1edab6fbf316e12348e5
VIEIGHT <118950924+VIEIGHT@users.noreply.github.com>
2022-10-26 07:04:29 +0000

    Run scan-local.sh on percona:...

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,4 +3,5 @@
   <div class="list-menu">
     <ul>
       <li><i class="bi bi-code-slash"></i> devloper</li>
-      <li><i class="bi 
+      <li><i class="bi bi-house"></i> Home</li>
+      <li><i class="bi bi-coin"></i> m
