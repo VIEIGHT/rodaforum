46d0b9a3e646ccc5ef390be973782f8fe3914a89
VIEIGHT <118950924+VIEIGHT@users.noreply.github.com>
2022-11-10 16:21:36 +0000

    Expressions

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -17,4 +17,5 @@
   <div class="list-links">
     <ul dir="rtl">
       <li>  <i class="bi bi-github"></i> Github</li>
-      <li> <i class="bi bi-stack-overflow has-text-warning-dark"></i
+      <li> <i class="bi bi-stack-overflow has-text-warning-dark"></i> stack overflow</li>
+      <li><i class="bi bi-linkedin has-
