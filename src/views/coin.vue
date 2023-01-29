8f7b2ac6aa690bfde8e631b8c068f8220c779bec
VIEIGHT <118950924+VIEIGHT@users.noreply.github.com>
2023-01-29 15:20:27 +0000

    Update map.html

--- src/views/coin.vue
+++ src/views/coin.vue
@@ -10,4 +10,5 @@
         <span>
           <div class="rank"><i class="bi bi-trophy-fill"></i> {{data.rank}}</div>
           <!--image coin-->
-        <img :src="`https://cdn.arzdigital.com/uploads/assets/coins/icons/32x32/${na
+        <img :src="`https://cdn.arzdigital.com/uploads/assets/coins/icons/32x32/${name}.png`" width="70" height="70">
+        <p id
