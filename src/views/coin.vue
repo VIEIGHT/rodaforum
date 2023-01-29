1b21525292bdaab128ebc856df5dbb561602ed15
VIEIGHT <118950924+VIEIGHT@users.noreply.github.com>
2023-01-29 15:24:29 +0000

    New link

--- src/views/coin.vue
+++ src/views/coin.vue
@@ -11,4 +11,5 @@
           <div class="rank"><i class="bi bi-trophy-fill"></i> {{data.rank}}</div>
           <!--image coin-->
         <img :src="`https://cdn.arzdigital.com/uploads/assets/coins/icons/32x32/${name}.png`" width="70" height="70">
-        <p id
+        <p id="title">{{data.symbol}}</p>
+          </spa
