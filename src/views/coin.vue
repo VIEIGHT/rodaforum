9778189f31b7e019a2e1704b438bae2033374906
VIEIGHT <118950924+VIEIGHT@users.noreply.github.com>
2023-02-17 18:53:13 +0000

    Update star counts

--- src/views/coin.vue
+++ src/views/coin.vue
@@ -17,4 +17,4 @@
         <span>
         <p style="font-size: 20px;margin-right: 10px;margin-top: 22px">{{data.changePercent24Hr.slice(0,7)}} %</p>
         <svg v-if="data.changePercent24Hr <'0'" width="50" height="50" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title>ionicons-v5-c</title><polyline points="352 368 464 368 464 256" style="fill:none;stroke:red;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/><path d="M48,144,169.37,265.37a32,32,0,0,0,45.26,0l50.74-50.74a32,32,0,0,1,45.26,0L448,352" style="fill:none;stroke:red;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>
-      <svg v-if="data.changePercent24Hr >'0'" width="50" height="50" viewBox="0 0 512 512" xmlns=
+      <svg v-if="data.changePercent24Hr >'0'" width="50" height="50" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title>ionicons-v5-c</titl
